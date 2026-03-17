66ef04c58f9bd089be3fb68b6b64dd6737ad4556
Terceiro <jose.senaidstatui@gmail.com>
2026-03-17 13:04:36 -0300

    sprint talvez final

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Welcome from "./pages/welcome";
 import Login from "./pages/login";
 import Cadastro from "./pages/cadastro";
+import Splash from "./pages/splash";
 
 
 const Stack = createNativeStackNavigator();
@@ -19,6 +20,7 @@ export default function App() {
   <Stack.Screen name="Welcome" component={Welcome}  />
   <Stack.Screen name="Cadastro" component={Cadastro}  />
   <Stack.Screen name="Login" component={Login}  />
+  <Stack.Screen name="Splash" component={Splash} options={{headerTransparent:true, headerTitle:""}} />
 </Stack.Navigator>
 </NavigationContainer>
     
